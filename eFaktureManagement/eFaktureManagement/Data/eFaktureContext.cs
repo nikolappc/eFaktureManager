a5334dbc7788e18183ee29ae0854cab7839b1f62
Nikola Papić <neekolapapic@gmail.com>
2024-06-18 22:07:31 +0200

    Done gitignore

---
+++ eFaktureManagement/eFaktureManagement/Data/eFaktureContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eFaktureManagement.Data
+{
+    public class eFaktureContext : IdentityDbContext
+    {
+        public eFaktureContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- eFaktureManagement/eFaktureManagement/Program.cs
+++ eFaktureManagement/eFaktureManagement/Program.cs
@@ -1,3 +1,6 @@
+using eFaktureManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var ConnectionString = builder.Configuration["Connnectionstrings:eFaktureConnection"];
+
+
+builder.Services.AddDbContext<eFaktureContext>(e=>e.UseNpgsql(ConnectionString));
+
 
 var app = builder.Build();
 
