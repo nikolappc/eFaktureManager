a8624dc7a01c2ebd28c35bb980d5f5492d69476b
Nikola Papić <neekolapapic@gmail.com>
2024-06-21 20:49:44 +0200

    Configured Vue

--- eFaktureManagementFrontEnd/e-fakture-management/src/App.vue
+++ eFaktureManagementFrontEnd/e-fakture-management/src/App.vue
@@ -1,9 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-layout>
+
+      <v-app-bar :elevation="2" color="lime-lighten-2">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+      
+        <v-app-bar-title >eFakture management</v-app-bar-title>
+   
+         <router-link to="/"><v-btn ><v-icon icon="mdi-home"></v-icon>Home </v-btn></router-link>
+         <router-link to="/about"><v-btn ><v-icon icon="mdi-information-slab-circle-outline"></v-icon>About </v-btn></router-link>
+         <v-spacer></v-spacer>
+
+
+      </v-app-bar>
+      <v-main>
+        <v-container fluid>
+          <router-view />
+
+        </v-container>
+      </v-main>
+    </v-layout>
+    <v-footer></v-footer>
+
+  </v-app>
 </template>
 
 <style lang="scss">
