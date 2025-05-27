6db5e25914c1cb3279d3fed646a6cefc61ac0c64
Nikola Papić <neekolapapic@gmail.com>
2025-05-27 21:21:29 +0200

    added tailwind css

--- eFaktureManagement/eFaktureModel/obj/Debug/net8.0/eFaktureModel.AssemblyInfo.cs
+++ eFaktureManagement/eFaktureModel/obj/Debug/net8.0/eFaktureModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eFaktureModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cd5ca89727e9b72929ebbbe2f04c0984b5dc12d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669d428a6bd4a8b9713ce92e31e9d90385640805")]
 [assembly: System.Reflection.AssemblyProductAttribute("eFaktureModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eFaktureModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- eFaktureManagementFrontEnd/e-fakture-management/src/main.js
+++ eFaktureManagementFrontEnd/e-fakture-management/src/main.js
@@ -8,6 +8,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import "./assets/output.css";
 
 const vuetify = createVuetify({
     components,
