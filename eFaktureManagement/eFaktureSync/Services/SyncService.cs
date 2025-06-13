70dac9b22ca9ce13d9a211b2eb3378a46e9f0b34
Nikola Papić <neekolapapic@gmail.com>
2025-06-14 00:27:19 +0200

    wip fetch changes

---
+++ eFaktureManagement/eFaktureSync/Services/SyncService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eFaktureSync.Services
+{
+    public class SyncService : ISyncService
+    {
+        private readonly IConfiguration configRoot;
+        public SyncService(IConfiguration configRoot)
+        {
+            this.configRoot = configRoot;
+        }
+
+        void Sync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
