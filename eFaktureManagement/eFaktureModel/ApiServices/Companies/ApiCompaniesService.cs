ddedcd461f6d9862565372bc7c0f418d9006a2fc
Nikola Papić <neekolapapic@gmail.com>
2025-07-30 21:19:33 +0200

    WIP Companies

---
+++ eFaktureManagement/eFaktureModel/ApiServices/Companies/ApiCompaniesService.cs
@@ -0,0 +1,37 @@
+﻿using eFaktureManagement.ApiModels;
+using eFaktureManagement.ApiModels.Purchase;
+using eFaktureManagement.ApiModels.Sale;
+using eFaktureModel.Api.Models;
+using eFaktureModel.ApiModels.Sale;
+using eFaktureModel.ApiServices.Companies;
+using eFaktureSync.ApiServices;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using System.Web;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using MiniInvoiceDto = eFaktureModel.Api.Models.MiniInvoiceDto;
+
+namespace eFaktureManagement.ApiServices
+{
+    public class ApiCompaniesService : IApiCompaniesService<MiniCompanyDto, CompanyAccountOnEfAkturaDto>
+    {
+        public Task<CompanyAccountOnEfAkturaDto> CheckIfCompanyRegisteredOnEfaktura(string registrationNumber, string jbkjs, string vatNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MiniCompanyDto>> GetAllCompaniesAsync(bool? includeAllStatuses = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCompanies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ eFaktureManagement/eFaktureModel/ApiServices/Companies/IApiCompaniesService.cs
@@ -0,0 +1,13 @@
+﻿using eFaktureModel.Api.Models;
+
+namespace eFaktureModel.ApiServices.Companies
+{
+    public interface IApiCompaniesService<C, R>
+    {
+        Task<List<C>> GetAllCompaniesAsync(bool? includeAllStatuses = false);
+
+        Task<R> CheckIfCompanyRegisteredOnEfaktura(string registrationNumber, string jbkjs, string vatNumber);
+
+        Task UpdateCompanies();
+    }
+}
