9b82f7ad6dcadeadde4e35542633fc93a65f6f0d
Nikola Papić <neekolapapic@gmail.com>
2026-03-12 22:12:13 +0100

    Added some datas to company

--- eFaktureManagement/eFaktureModel/Model/Company/RegisteredCompany.cs
+++ eFaktureManagement/eFaktureModel/Model/Company/RegisteredCompany.cs
@@ -1,9 +1,19 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+
 namespace eFaktureModel.Model.Company
 {
 
-    public partial class RegisteredCompany { 
+    public partial class RegisteredCompany {
+
+
+        [Key]
+        public int Id   { get; set; }
+
+
+        // Useful to connect with the id in eFakture system
+        public int? eFaktureCompanyId { get; set; }
 
 
         public string BugetCompanyNumber { get; set; }
@@ -13,11 +23,18 @@ namespace eFaktureModel.Model.Company
         public string VatRegistrationCode { get; set; }
 
         public string Name { get; set; }
+        public int? CountryId { get; set; }
+
+        
 
         public DateTime? RegistrationDate { get; set; }
 
-
         public DateTime? DeletionDate { get; set; }
+
+
+        // efakture data might be usefull
+        public bool? IsPrivateCompany { get; set; }
+
 
     }
 }
