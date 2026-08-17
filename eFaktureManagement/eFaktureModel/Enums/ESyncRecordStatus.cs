5935ab141b5a493e995f39c6cde38e7b19d4dd02
Nikola Papić <neekolapapic@gmail.com>
2026-08-17 21:00:25 +0200

    Sync check added

---
+++ eFaktureManagement/eFaktureModel/Enums/ESyncRecordStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eFaktureModel.Enums
+{
+    public enum ESyncRecordStatus
+    {
+        CHECKED, APPLIED
+    }
+}
---
+++ eFaktureManagement/eFaktureModel/Model/Sync/AInvoiceChangeRecord.cs
@@ -0,0 +1,22 @@
+﻿using eFaktureModel.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eFaktureModel.Model.Sync
+{
+    public abstract class AInvoiceChangeRecord
+    {
+        [Key]
+        public int Id { get; set; }
+        public ESyncRecordStatus RecordStatus{ get; set; }
+        public ESyncType SyncType { get; set; }
+
+        public DateTime RecordedOn { get; set; }
+
+
+    }
+}
